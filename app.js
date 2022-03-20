da34b1d3a17eeede32494cfa83d0fa5f405e8108
yogeshkumar2001 <yk9599493681@gmail.com>
2022-03-20 19:13:06 +0530

    change

--- app.js
+++ app.js
@@ -2,35 +2,32 @@ const express = require("express");
 
 // application // backend // server
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.use( express.static("public") );
+app.use(express.static("public"));
 
+io.on("connection", function (socket) {
+  console.log(socket.id + " connected !! ");
 
-io.on('connection', function(socket){
-   console.log(socket.id + " connected !! ");
-
-   socket.on("md" , function(pointObject){
+  socket.on("md", function (pointObject) {
     //    console.log("md" , pointObject);
     socket.broadcast.emit("mousedown", pointObject);
-   })
-
-   socket.on("mm" , function(pointObject){
-    //    console.log("mm" , pointObject);
-    socket.broadcast.emit("mousemove", pointObject);
-   })
-
-   socket.on("mu" , function(){
-       socket.broadcast.emit("mouseup");
-   })
-
-
-   socket.on("disconnect" , function(){
-       console.log("socket disconnected !!");
-   })
   });
 
+  socket.on("mm", function (pointObject) {
+    //    console.log("mm" , pointObject);
+    socket.broadcast.emit("mousemove", pointObject);
+  });
+
+  socket.on("mu", function () {
+    socket.broadcast.emit("mouseup");
+  });
+
+  socket.on("disconnect", function () {
+    console.log("socket disconnected !!");
+  });
+});
 
 // http method , app route , route handler
 // app.get("/" , function(request , response){
@@ -39,8 +36,6 @@ io.on('connection', function(socket){
 //     response.sendFile(__dirname+"/public/main.html");
 // })
 
-
-
-http.listen(3000 , function(){
-    console.log("app started at port 3000 !!!");
-})
+http.listen(`https://yogeshkumar2001.github.io/White-Board/`, function () {
+  console.log("app started at port 3000 !!!");
+});
--- public/sticky.js
+++ public/sticky.js
@@ -1,12 +1,4 @@
-{/* <div class="sticky">
-<div class="sticky-nav">
-    <div class="minimize"></div>
-    <div class="close"></div>
-</div>
-<div class="sticky-content">
-    <textarea id="sticky-text"></textarea>
-</div>
-</div> */}
+
 
 let stickyIcon = document.querySelector("#sticky");
 
